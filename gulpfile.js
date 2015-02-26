d2db74493195227816754183a13d4e50c82a315f
alu0100315462 <alu0100315462@ull.edu.es>
2015-02-26 19:47:49 +0000

    automatizando karma a traves de gulp

--- gulpfile.js
+++ gulpfile.js
@@ -23,3 +23,24 @@ gulp.task('minify', function () {
 gulp.task('clean', function(cb) {
   del(['minified/*'], cb);
 });
+
+gulp.task('test', function() {
+  // Be sure to return the stream
+  return gulp.src([])
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'run'
+    }))
+    .on('error', function(err) {
+      // Make sure failed tests cause gulp to exit non-zero
+      throw err;
+    });
+});
+
+gulp.task('default', function() {
+  gulp.src([])
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'watch'
+    }));
+});
